cff2be73161d94172589c8b11a967acee9ae0140
soubj <sobujahmed124@gmail.com>
2024-02-14 20:54:09 +0600

    utils code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+const port = process.env.PORT || 5000;
+
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("err : ", err);
+      throw err;
+    });
+
+    app.listen(port, () => {
+      console.log(`server is running at port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`mongodb connection failed ! `, err);
+  });
 
 /*
 This is one of the way to connect with database.In this way we write the database connection inside index file.
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// the same code can be written this way
+/*
+const asyncHandler = (func) => async (req, res, next) => {
+  try {
+    await func(req, res, next);
+  } catch (error) {
+    res.status(err.code || 500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+*/
