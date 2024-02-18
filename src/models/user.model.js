f7ff00f94b1a287810b06260afdfcc6e84be3697
soubj <sobujahmed124@gmail.com>
2024-02-18 12:54:29 +0600

    multer and cloudinary file upload

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -53,7 +53,7 @@ const userSchema = new mongoose.Schema(
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
@@ -88,4 +88,5 @@ userSchema.methods.generateRefreshToken = function () {
     }
   );
 };
+
 export const User = mongoose.model("User", userSchema);
