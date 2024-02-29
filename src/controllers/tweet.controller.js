4ae8aab561d44e2aed070cfe4e4af6cafb2bd4df
soubj <sobujahmed124@gmail.com>
2024-02-29 14:41:03 +0600

    add getUserTweets endpoint

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -22,6 +22,24 @@ const createTweet = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, tweet, "Tweet created successfully."));
 });
 
+const getUserTweets = asyncHandler(async (req, res) => {
+  const { userId } = req.params;
+  if (!userId) {
+    throw new ApiError(400, "User is required.");
+  }
+
+  const userTweets = await Tweet.find({ owner: userId });
+  if (!userTweets?.length) {
+    throw new ApiError(404, "No tweets found.");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, userTweets, "user tweets fetched successfully.")
+    );
+});
+
 const updateTweet = asyncHandler(async (req, res) => {
   const { tweetId } = req.params;
   const { content } = req.body;
@@ -65,4 +83,4 @@ const deleteTweet = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, deletedTweet, "Tweet deleted successfully."));
 });
 
-export { createTweet, updateTweet, deleteTweet };
+export { createTweet, updateTweet, deleteTweet, getUserTweets };
--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -3,6 +3,7 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
   createTweet,
   deleteTweet,
+  getUserTweets,
   updateTweet,
 } from "../controllers/tweet.controller.js";
 
@@ -11,7 +12,8 @@ const router = Router();
 router.use(verifyJWT);
 
 router.route("/").post(createTweet); //tested
-router.route("/update/:tweetId").patch(updateTweet);
-router.route("/delete/:tweetId").delete(deleteTweet);
+router.route("/update/:tweetId").patch(updateTweet); //tested
+router.route("/delete/:tweetId").delete(deleteTweet); //tested
+router.route("/user/:userId").get(getUserTweets); //tested
 
 export default router;
