2f4f33e2fffd1d30b01072511785ae48c93e4bd6
soubj <sobujahmed124@gmail.com>
2024-03-03 20:19:31 +0600

    add toggleCommentLike endpoint

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -15,6 +15,7 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
       {
         video: videoId,
         likedBy: req.user._id,
+        $or: [{ comment: { $exists: true } }, { tweet: { $exists: true } }],
       },
       {
         $unset: {
@@ -29,6 +30,17 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
       return res
         .status(200)
         .json(new ApiResponse(200, removeLike, "video like removed."));
+    }
+
+    const deleteDoc = await Like.findOneAndDelete({
+      video: videoId,
+      likedBy: req.user._id,
+      $and: [{ comment: { $exists: false } }, { tweet: { $exists: false } }],
+    });
+    if (deleteDoc) {
+      return res
+        .status(200)
+        .json(new ApiResponse(200, deleteDoc, "Toggled video likes."));
     }
 
     const likeVideo = await Like.create({
@@ -49,4 +61,99 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
   }
 });
 
-export { toggleVideoLike };
+const toggleCommentLike = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(400, "Provide valid comment id.");
+  }
+
+  try {
+    const removeComment = await Like.findOneAndUpdate(
+      {
+        comment: commentId,
+        likedBy: req.user._id,
+        $or: [{ tweet: { $exists: true } }, { video: { $exists: true } }],
+      },
+      {
+        $unset: {
+          comment: true,
+        },
+      },
+      {
+        new: true,
+      }
+    );
+    if (removeComment) {
+      return res
+        .status(200)
+        .json(
+          new ApiResponse(
+            200,
+            removeComment,
+            "comment like field removed successfully."
+          )
+        );
+    }
+
+    const deleteDoc = await Like.findOneAndDelete({
+      comment: commentId,
+      likedBy: req.user._id,
+      $and: [{ video: { $exists: false } }, { tweet: { $exists: false } }],
+    });
+    if (deleteDoc) {
+      return res
+        .status(200)
+        .json(
+          new ApiResponse(
+            200,
+            deleteDoc,
+            "Toggled comment likes by deleting document."
+          )
+        );
+    }
+
+    const findDoc = await Like.findOneAndUpdate(
+      {
+        likedBy: req.user._id,
+        $or: [{ video: { $exists: true } }, { tweet: { $exists: true } }],
+      },
+      {
+        $set: {
+          comment: commentId,
+        },
+      },
+      {
+        new: true,
+      }
+    );
+    if (findDoc) {
+      return res
+        .status(200)
+        .json(
+          new ApiResponse(
+            200,
+            findDoc,
+            "Commend like toggled by setting comment field successfully."
+          )
+        );
+    }
+
+    const likeComment = await Like.create({
+      comment: commentId,
+      likedBy: req.user._id,
+    });
+    if (!likeComment) {
+      throw new ApiError(500, "Comment like failed.");
+    }
+    return res
+      .status(200)
+      .json(new ApiResponse(200, likeVideo, "Comment liked successfully."));
+  } catch (error) {
+    console.log("err : ", error);
+    return res
+      .status(error.statusCode || 500)
+      .json(new ApiResponse(error.statusCode || 500, {}, error?.message));
+  }
+});
+
+export { toggleVideoLike, toggleCommentLike };
--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { toggleVideoLike } from "../controllers/like.controller.js";
+import {
+  toggleCommentLike,
+  toggleVideoLike,
+} from "../controllers/like.controller.js";
 
 const router = Router();
 
 router.use(verifyJWT);
 
 router.route("/toggle/video/:videoId").post(toggleVideoLike);
+router.route("/toggle/comment/:commentId").post(toggleCommentLike);
 
 export default router;
